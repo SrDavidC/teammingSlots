82b1b694c1826ff97bf03c5f1225c887d259fc24
BuildTools <unconfigured@null.spigotmc.org>
2023-04-22 18:19:47 -0600

    feature: Game method changed

--- src/main/java/srdqrk/teammingslots/game/Game.java
+++ src/main/java/srdqrk/teammingslots/game/Game.java
@@ -1,15 +1,24 @@
 package srdqrk.teammingslots.game;
 
 import lombok.Data;
+import lombok.Getter;
 import srdqrk.teammingslots.TeammingSlots;
 
-@Data
-public class Game {
 
-  GameStateEnum gameState;
-  TeammingSlots instance;
+public class Game {
+  @Getter
+  private GameStateEnum gameState;
+  private TeammingSlots instance;
   public Game(TeammingSlots instance, GameStateEnum gameState) {
     this.instance = instance;
     this.gameState = gameState;
   }
+
+  public void startMatch() {
+    this.gameState = GameStateEnum.IN_MATCH;
+  }
+  public void startSlots() {
+    this.gameState = GameStateEnum.IN_SLOTS;
+  }
+
 }
