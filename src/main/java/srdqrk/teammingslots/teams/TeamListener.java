7f44b4a291cafbb38b48286926417507c7ab43c2
DavidC <david.cerdasalvarado@ucr.ac.cr>
2023-05-23 13:14:09 -0600

    fix: on PlayerDeath Event now removes from the list participants if player death in 'world'

--- src/main/java/srdqrk/teammingslots/teams/TeamListener.java
+++ src/main/java/srdqrk/teammingslots/teams/TeamListener.java
@@ -18,47 +18,50 @@ import srdqrk.teammingslots.game.GameStateEnum;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TeamListener  implements Listener {
+public class TeamListener implements Listener {
 
-    @Dependency
-    TeammingSlots instance;
-    FileConfiguration configFile;
-    public TeamListener(TeammingSlots instance) {
-        this.instance = instance;
-        this.configFile = instance.getConfig();
+  @Dependency
+  TeammingSlots instance;
+  FileConfiguration configFile;
+
+  public TeamListener(TeammingSlots instance) {
+    this.instance = instance;
+    this.configFile = instance.getConfig();
+  }
+
+  @EventHandler
+  public void onDeath(PlayerDeathEvent e) {
+    // Check if the player death occurred in the correct world
+    if (!e.getEntity().getWorld().getName().equals("world")) {
+      return;
     }
 
-    @EventHandler
-    public void onDeath(PlayerDeathEvent e) {
-        // if the players aren't on slots, do nothing
-        if (this.instance.getGame().getGameState() != GameStateEnum.IN_SLOTS) {
-            return;
-        }
+    Player player = e.getEntity();
+    CommandSender sender = Bukkit.getConsoleSender();
 
-        Player player = e.getEntity();
-        CommandSender sender = Bukkit.getConsoleSender();
-        // If the player is not in 'participantes', notify
-        if ((this.configFile.getList("participantes") != null && this.configFile.getList("participantes").contains(player.getName()))) {
-            sender.sendMessage(ChatColor.RED + "El jugador " + player.getName() + " no se encuentra en la lista de participantes.");
-            return;
-        }
-        // Delete from participantes and add to noParticipantes
-        List<String> participantes = this.configFile.getStringList("participantes");
-        List<String> noParticipantes = this.configFile.getStringList("noParticipantes");
-        participantes.remove(player.getName());
-        noParticipantes.add(player.getName());
-        this.configFile.set("participantes", participantes);
-        this.configFile.set("noParticipantes", participantes);
-        // save config
-        this.instance.saveConfig();
-        // Broadcast to all staffs
-        for (Player staffPlayer : Bukkit.getOnlinePlayers()) {
-            if (staffPlayer.hasPermission("teammingslots.executer")) {
-                staffPlayer.sendMessage(ChatColor.YELLOW + "[INFO]" + ChatColor.RED + " El jugador " + player.getName() +
-                        " ha sido eliminado");
-            }
-        }
-        sender.sendMessage(ChatColor.GREEN + "Se ha eliminado al jugador " + player.getName() + " de la lista de participantes");
+    // Check if the player is in the 'participantes' list
+    List<String> participantes = this.configFile.getStringList("participantes");
+    if (!(participantes.contains(player.getName()))) {
+      sender.sendMessage(ChatColor.RED + "El jugador " + player.getName() + " no se encuentra en la lista de participantes.");
+      return;
     }
+
+    // Remove player from 'participantes' and add to 'noParticipantes'
+    participantes.remove(player.getName());
+    List<String> noParticipantes = this.configFile.getStringList("noParticipantes");
+    noParticipantes.add(player.getName());
+
+    // Update the config
+    this.configFile.set("participantes", participantes);
+    this.configFile.set("noParticipantes", noParticipantes);
+
+    // Save the config
+    this.instance.saveConfig();
+
+    // Broadcast to all players with 'teammingslots.executer' permission
+    TeammingSlots.instance().logStaff(ChatColor.RED + "El jugador " + player.getName() +
+            " ha sido eliminado.");
+  }
+
 
 }
